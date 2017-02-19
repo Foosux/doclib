de251f31fa0b5bef87109af44b03077529c3fa18
zhumin <zhumin@lianjia.com>
2017-02-19 23:07:06 +0800

    modify 重新调整左侧的导出结构

--- conf/files.js
+++ conf/files.js
@@ -15,25 +15,38 @@ function readFile(searchStartPath) {
   var files = fs.readdirSync(searchStartPath)
   // 获取文件长度，用于判断是否文件夹内最后一个元素
   var count = files.length
+  // console.log(count)
   // 用于存储元数据
   var results = {}
   var relativePath = path.relative(docsRoot, searchStartPath)
   // console.log(docsRoot, '\n',searchStartPath ,'\n', relativePath)
   // 文件分类及排序
-  var sortFiles = fileSort(files, searchStartPath)
+  var sortFilesJson = fileSort(files)
+  // console.log(sortFilesJson)
   // 遍历
-  sortFiles.forEach(function (filename) {
+  sortFilesJson.filesArr.forEach(function (filename,i) {
     //docsRoot+"/"+filename不能用/直接连接，Unix系统是”/“，Windows系统是”\“
     var stats = fs.statSync(path.join(searchStartPath, filename))
     //文件
     if (stats.isFile()) {
       if (path.extname(filename) == '.md') {
-        var newLink='<dd><a href="/md/'+path.basename(filename,'.md')+'?path='+path.join('',relativePath)+''+'">'+path.basename(filename,'.md') +'</a></dd>'
+        // 补充ol结构的结尾部分
+        var lastHtml=''
+        // 没有嵌套结构后的最后一个 dd 需要补充前面的 ol(嵌套多少层补充多少个)
+        if (sortFilesJson.dirCount==0 && i==count-sortFilesJson.dirCount-1) {
+          for(var i=1; i<=relativePath.split('/').length; i++){
+            lastHtml += '</ol>'
+          }
+        } else {
+          lastHtml = ''
+        }
+        var newLink = '<dd><a href="/md/'+path.basename(filename,'.md')+'?path='+relativePath+'">'+path.basename(filename,'.md') +'</a></dd>'+lastHtml
         fileArr.push(newLink)
         writeFile(fileArr,filename)
       }
     } else if (stats.isDirectory()) {
-      var newDir = '<dt>'+filename+'</dt>'
+      // console.log(sortFilesJson.firstDirName)
+      var newDir =  '<ol><dt>'+filename+'</dt>'
       fileArr.push(newDir)
       writeFile(fileArr,filename)
       // 遍历子目录
@@ -45,15 +58,19 @@ function readFile(searchStartPath) {
 // 排序 文件夹放置在最后
 function fileSort (files) {
   var filesArr = files
+  var dirCount=0
   files.forEach(function (filename,i) {
     if (!path.extname(filename)) {
+      dirCount++
       var del = filesArr.splice(i,1)
       filesArr.push(del[0])
     }
   })
-  return filesArr
+  return {
+    filesArr: filesArr,
+    dirCount: (dirCount-1 >= 0) ? dirCount-1 : 0
+  }
 }
-
 
 // 写入到filelisttxt文件
 function writeFile(data,failname){
