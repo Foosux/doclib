e7219ecf4e850f144c9d25e2191ae676e99330ac
zhumin <zhumin@lianjia.com>
2017-02-21 11:11:54 +0800

    add 新增用户管理浮层

--- public/js/adminUserManage.js
+++ public/js/adminUserManage.js
@@ -1,3 +1,11 @@
+/**
+ * 后台-用户管理
+ */
 $(function(){
-
+  $('.actClose').click(function(){
+    $(this).parents('.G-layerWrap').hide()
+  })
+  $('.actAddUser').click(function(){
+    $('.G-layerWrap').show()
+  })
 })
